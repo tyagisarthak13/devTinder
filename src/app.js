3ffd25c20cf7b2edd15ebc27aff813bb0bd68072
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-02-13 12:14:33 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -4,67 +4,37 @@ const express = require("express");
 
 const app = express();
 
-//! These are called route handlers
-//! Order of these routes matters a lot
-// app.use("/user", (req, res) => {
-//   res.send("HAHAHAHAHAHAHAHA");
-// });
-
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "Harry", lastName: "Kane" });
-});
-
-//! Dynamic route
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Harry", lastName: "Kane" });
-});
-
-//! Also a dynamic route
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Harry", lastName: "Kane" });
-});
-
-// This will only handle GET call to/user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Akshay", lastName: "Saini" });
-});
-
-// Means that b is optional
-app.get("/ab?c", (req, res) => {
-  res.send({ firstName: "Cristiano", lastName: "Ronaldo" });
-});
-
-//! Can group also, here bc should be optional
-// app.get("/a(bc)?d", (req, res) => {
-//   res.send({ firstName: "Cristiano", lastName: "Ronaldo" });
-// });
-
-// Means that a should be at first and c should be at last, in middle it could be anything
-app.use("/ab+c", (req, res) => {
-  res.send({ firstName: "James", lastName: "Maddison" });
-});
-
-// Means that ab should be at start and cs should at last in between it could be anything
-app.use("/ab*cd", (req, res) => {
-  res.send({ firstName: "Cristian", lastName: "Romero" });
-});
-
-app.post("/user", (req, res) => {
-  // saving data to DB
-  res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-// This will match all the HTTPmethod API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
+// A route can have multiple route handlers.
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      // Route Handler
+      console.log("Handling the route user!!");
+      // res.send("Response!!");
+      next();
+    },
+    (req, res, next) => {
+      // Route Handler 2
+      console.log("Handling the route user 2 ");
+      next();
+      // res.send("2nd Response!!");
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handling the route user 3");
+    next();
+    // res.send("3rd Response");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4");
+    res.send("4th Response");
+  }
+);
+// We can also send these callback functions in arrays by enclosing them inside []
+// app.use("/route", [RH1, RH2, RH3], RH4, RH5 )
+// app.use("/route", [RH1, RH2, RH3, RH4, RH5] )
+// app.use("/route", RH1, RH2, [RH3], RH4, RH5 )
 
 // Below callback function will only be called when our server is up an runnung(successfully running).
 app.listen(7777, () => {
