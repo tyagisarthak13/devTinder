4c5a40bba1e86d11e6fd6948a5de6014a34df6a9
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-02-13 15:34:32 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -4,32 +4,26 @@ const express = require("express");
 
 const app = express();
 
-// Express takes and matches the route by going through the middlewares one after the other till it sends the response back.
-// The only job of express is to take the request and give the response ASAP, and it goes in order.
-app.use("/", (req, res, next) => {
-  // res.send("Handling /route");
-  next();
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+// Handle Auth Middleware for all HTTP REQUESTS
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req, res) => {
+  res.send("User logged in successfully");
 });
 
-app.use("/user", (req, res, next) => {
-  console.log("Handling the route user!!");
-  // res.send("Response!!");
-  next();
+app.get("/user", userAuth, (req, res) => {
+  res.send("User Data Sent");
 });
 
-// These also works exactly same as calling one route handle inside another
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the route user!!");
-    // res.send("Response 2!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user");
-    res.send("Response Received!!!");
-  }
-);
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a user");
+});
 
 // Below callback function will only be called when our server is up an runnung(successfully running).
 app.listen(7777, () => {
