eb1833e6f83cd6e7226fa6366fa01e553a0b7015
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-02-13 12:37:57 +0530

    multiple route handlers pt2

--- src/app.js
+++ src/app.js
@@ -4,37 +4,32 @@ const express = require("express");
 
 const app = express();
 
-// A route can have multiple route handlers.
+// Express takes and matches the route by going through the middlewares one after the other till it sends the response back.
+// The only job of express is to take the request and give the response ASAP, and it goes in order.
+app.use("/", (req, res, next) => {
+  // res.send("Handling /route");
+  next();
+});
+
+app.use("/user", (req, res, next) => {
+  console.log("Handling the route user!!");
+  // res.send("Response!!");
+  next();
+});
+
+// These also works exactly same as calling one route handle inside another
 app.use(
   "/user",
-  [
-    (req, res, next) => {
-      // Route Handler
-      console.log("Handling the route user!!");
-      // res.send("Response!!");
-      next();
-    },
-    (req, res, next) => {
-      // Route Handler 2
-      console.log("Handling the route user 2 ");
-      next();
-      // res.send("2nd Response!!");
-    },
-  ],
   (req, res, next) => {
-    console.log("Handling the route user 3");
+    console.log("Handling the route user!!");
+    // res.send("Response 2!!");
     next();
-    // res.send("3rd Response");
   },
   (req, res, next) => {
-    console.log("Handling the route user 4");
-    res.send("4th Response");
+    console.log("Handling the route user");
+    res.send("Response Received!!!");
   }
 );
-// We can also send these callback functions in arrays by enclosing them inside []
-// app.use("/route", [RH1, RH2, RH3], RH4, RH5 )
-// app.use("/route", [RH1, RH2, RH3, RH4, RH5] )
-// app.use("/route", RH1, RH2, [RH3], RH4, RH5 )
 
 // Below callback function will only be called when our server is up an runnung(successfully running).
 app.listen(7777, () => {
