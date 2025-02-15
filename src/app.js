1ccc33d3860d4da186c281caadf6aafa8f786910
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-02-15 16:50:02 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -18,6 +18,33 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//! get user by email(more than one user can be found)
+// to find one user use Model.findone
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+//! Feed API = GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 //! NOTE - Before you start your application your database should be cpnnected and that is why we have done app.listen inside
 connectDB()
   .then(() => {
